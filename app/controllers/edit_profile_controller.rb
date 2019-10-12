2df44eec01ce3d4b12758caa700275280df849ed
roi <roiruby0@gmail.com>
2019-10-12 04:26:46 +0000

    currentuser

--- app/controllers/edit_profile_controller.rb
+++ app/controllers/edit_profile_controller.rb
@@ -1,4 +1,6 @@
 class EditProfileController < ApplicationController
+  before_action :require_user_logged_in, only: [:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
   
   def edit
     @user = User.find(params[:id])
@@ -20,6 +22,11 @@ class EditProfileController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,)
+  end
+  
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
   
   def index
   end
@@ -48,6 +49,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :catchphrase, :introduce, :image, :remove_image)
+  end
+  
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
   end
   
 end
