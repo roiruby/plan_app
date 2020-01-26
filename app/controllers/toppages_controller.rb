c0c087696ef7b88995354af2032fdbeb0fe04b57
roi <roiruby0@gmail.com>
2020-01-26 16:25:01 +0000

    android_error

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -26,6 +26,8 @@ class ToppagesController < ApplicationController
   
   def devise_variant
       case request.user_agent
+      # when /iPad/
+      #   request.variant = :tablet
       when /iPhone/
         request.variant = :mobile
       when /android/
