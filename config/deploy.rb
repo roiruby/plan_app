9edf798937bb1640b97a94343aece376a61b601b
roi <roiruby0@gmail.com>
2020-01-26 15:27:12 +0000

    seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,17 @@ task :db_create do
   end
 end
 
+desc 'db_seed_fu'
+task :db_seed_fu do
+  on roles(:db) do |host|
+    with rails_env: fetch(:rails_env) do
+      within current_path do
+        execute :bundle, :exec, :rake, 'db:seed_fu'
+      end
+    end
+  end
+end
+
 
 
 after :publishing, :restart
@@ -49,15 +60,3 @@ after :publishing, :restart
   end
 end
 
-namespace :db do
-  desc 'Load seed data into database'
-  task :seed_fu do
-    on roles(:app) do
-      within current_path do
-        with rails_env: fetch(:rails_env) do
-          execute :bundle, :exec, :rails, 'db:seed_fu'
-        end
-      end
-    end
-  end
-end
