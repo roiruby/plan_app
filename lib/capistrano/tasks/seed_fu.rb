df3181edf72121a3dc8f7e12a6db4ded47259466
roi <roiruby0@gmail.com>
2020-01-26 11:03:36 +0000

    seed

--- lib/capistrano/tasks/seed_fu.rb
+++ lib/capistrano/tasks/seed_fu.rb
@@ -2,7 +2,7 @@ namespace :db do
   desc 'Load seed data into database'
   task :seed_fu do
     on roles(:app) do
-      within release_path do
+      within current_path do
         with rails_env: fetch(:rails_env) do
           execute :bundle, :exec, :rails, 'db:seed_fu'
         end
