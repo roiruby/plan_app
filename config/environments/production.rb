99825ff41172ae3418f29f805f0995769745acfb
roi <roiruby0@gmail.com>
2020-01-28 14:08:13 +0000

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pladuce1_0_#{Rails.env}"
   
-  
 
   config.action_mailer.perform_caching = false
 
