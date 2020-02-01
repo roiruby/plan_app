cf5a5f97e2df657ee4210ffc63d663c490a8d37c
roi <roiruby0@gmail.com>
2020-02-01 07:06:34 +0000

    time

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Pladuce10
     config.i18n.default_locale = :ja
     
     config.time_zone = 'Asia/Tokyo'
-    config.active_record.default_timezone = :local
+    # config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
