53c9f8123fa84cf1fce61f24cb88eeb6cd29aecd
roi <roiruby0@gmail.com>
2020-02-01 08:24:47 +0000

    googleanaly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,5 +108,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  GA.tracker = "UA-157537683-1"
+  
   
 end
