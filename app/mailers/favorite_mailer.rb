cd867a2416e4fded0ce074b5e246efd2004519cf
roi <roiruby0@gmail.com>
2020-02-03 10:47:11 +0000

    validate

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,6 +2,6 @@ class FavoriteMailer < ApplicationMailer
   def favorite_notification(user, plan)
     @user = user
     @plan = plan
-    mail to: plan.user.email, subject: "【Pladuce】お気に入りに追加されました！】"
+    mail to: plan.user.email, subject: "【Pladuce】お気に入りに追加されました！"
   end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,14 +4,14 @@ class Schedule < ApplicationRecord
   validates :schedule_title, length: { maximum: 50 }
   validates :sub_title, length: { maximum: 50 }
   validates :content, length: { maximum: 700 }
-  validates :spot_name, length: { maximum: 50 }
-  validates :address, length: { maximum: 50 }
+  validates :spot_name, length: { maximum: 100 }
+  validates :address, length: { maximum: 200 }
   validates :access, length: { maximum: 255 }
-  validates :business_hour, length: { maximum: 200 }
-  validates :regular_holiday, length: { maximum: 100 }
-  validates :tel, length: { maximum: 50 }
-  validates :parking, length: { maximum: 50 }
-  validates :budget, length: { maximum: 200 }
+  validates :business_hour, length: { maximum: 255 }
+  validates :regular_holiday, length: { maximum: 255 }
+  validates :tel, length: { maximum: 200 }
+  validates :parking, length: { maximum: 255 }
+  validates :budget, length: { maximum: 255 }
   validates :comment, length: { maximum: 255 }
   
   mount_uploader :image1, ImageUploader
